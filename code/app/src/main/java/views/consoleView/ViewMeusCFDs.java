aeb23d67b91cfdd2fd9645e7b8f594889e7b1b90
Luís Martins <lguilhermem@hotmail.com>
2020-01-12 12:51:03 +0000

    exemplo2LongMethod

--- code/app/src/main/java/views/consoleView/ViewMeusCFDs.java
+++ code/app/src/main/java/views/consoleView/ViewMeusCFDs.java
@@ -27,7 +27,46 @@ public class ViewMeusCFDs extends ConsoleView {
         nextMenu(option);
     }
 
+    private boolean apresentaContratosAtualizados() {
+        if(isUpdated()){
+            boolean yes = yesOrNoQuestion("Alguns dos seus contratos foram atualizados\nQuer dar refresh?");
+            if(yes){
+                mediator.changeView(MEUS_CFDS);
+                return true;
+            }
+        }
+        return false;
+    }
 
+    private boolean isaCFDOption(int option) {
+        return option > 0 && option <= cfds.size();
+    }
+
+    private int getOption() {
+        int option = 0;
+        boolean optionSelected = false;
+        while (!optionSelected){
+            String input = scanner.nextLine();
+            if(input.matches("[0-9]+")){
+                option = Integer.parseInt(input);
+                optionSelected = true;
+            }
+            else verifyPage(input);
+        }
+        return option;
+    }
+
+    private void verifyPage(String input) {
+        if(input.matches("[ ]*:[ ]*page[ ]+[0-9]+[ ]*")){
+            Pattern pattern = Pattern.compile("[ ]*:[ ]*page[ ]+([0-9]+)[ ]*");
+            Matcher matcher = pattern.matcher(input);
+            if(matcher.find()) {
+                int pageNumber = Integer.parseInt(matcher.group(1));
+                System.out.println("0.Retroceder");
+                printPage(pageNumber, cfds);
+            }
+        }
+    }
 
     private void nextMenu(int option) {
         if(isaCFDOption(option)){
@@ -41,40 +80,4 @@ public class ViewMeusCFDs extends ConsoleView {
         }
     }
 
-    private boolean isaCFDOption(int option) {
-        return option > 0 && option <= cfds.size();
-    }
-
-    private boolean apresentaContratosAtualizados() {
-        if(isUpdated()){
-            boolean yes = yesOrNoQuestion("Alguns dos seus contratos foram atualizados\nQuer dar refresh?");
-            if(yes){
-                mediator.changeView(MEUS_CFDS);
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    private int getOption() {
-        int option = 0;
-        boolean optionSelected = false;
-        while (!optionSelected){
-            String input = scanner.nextLine();
-            if(input.matches("[0-9]+")){
-                option = Integer.parseInt(input);
-                optionSelected = true;
-            } else if(input.matches("[ ]*:[ ]*page[ ]+[0-9]+[ ]*")){
-                Pattern pattern = Pattern.compile("[ ]*:[ ]*page[ ]+([0-9]+)[ ]*");
-                Matcher matcher = pattern.matcher(input);
-                if(matcher.find()) {
-                    int pageNumber = Integer.parseInt(matcher.group(1));
-                    System.out.println("0.Retroceder");
-                    printPage(pageNumber, cfds);
-                }
-            }
-        }
-        return option;
-    }
 }
